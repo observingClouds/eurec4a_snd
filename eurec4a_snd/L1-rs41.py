185d77661b0140da1f8fc652f02b3a9cfd7a127f
observingClouds <43613877+observingClouds@users.noreply.github.com>
2019-10-08 11:49:43 -0700

    \ PEP8

--- eurec4a_snd/L1-rs41.py
+++ eurec4a_snd/L1-rs41.py
@@ -7,8 +7,12 @@ __author__ = "Johannes Kiliani/Lukas Frank"
 __date__ = "$Nov 19, 2018 05:27:06 PM$"
 
 # insert some subroutines if possible
-import time, datetime, calendar
-import os.path, sys, glob
+import time
+import datetime
+import calendar
+import os.path
+import sys
+import glob
 import numpy as np
 from netCDF4 import Dataset, default_fillvals, num2date
 import subprocess as sp
@@ -83,7 +87,8 @@ def get_args():
     parsed_args = vars(parser.parse_args())
 
     if (parsed_args["date"] is None) and (parsed_args["inputfile"] is None):
-        parser.error("either --date or --inputfile needs to be set. (--date not yet implemented)")
+        parser.error(
+            "either --date or --inputfile needs to be set. (--date not yet implemented)")
 
     return parsed_args
 
@@ -102,7 +107,8 @@ if args["outputfolder"] is None:
     args["outputfolder"] = config["CONVERT_BCO_RADIOSONDES"]["OUTPUT_PATH"]
 
 try:
-    git_module_version = sp.check_output(["git", "describe", "--always"]).strip()
+    git_module_version = sp.check_output(
+        ["git", "describe", "--always"]).strip()
 except:
     git_module_version = "--"
 
@@ -110,20 +116,20 @@ compression_level = int(args["compression"])
 
 time_in = time.time()
 
-#<<<<<<< L1-rs41.py
+# <<<<<<< L1-rs41.py
 # Creating file list according to given arguments
 if args['inputfile'] is None:
     filelist = glob.glob(args['inputpath'] + '*.txt')
 else:
     filelist = glob.glob(args['inputfile'])
 filelist = sorted(filelist)
-#=======
+# =======
 inroot = '/media/lukas/Lidar_LF/Radiosondes/ASCII/201902/'
 outroot = '/media/lukas/Lidar_LF/Radiosondes/NetCDF/'
-    
+
 filelist0 = os.listdir(inroot)
 filelist0.sort()
-#>>>>>>> L1-rs41.py
+# >>>>>>> L1-rs41.py
 
 print('Files to process {}'.format(filelist))
 for ifile in range(0, len(filelist)):
@@ -138,7 +144,8 @@ for ifile in range(0, len(filelist)):
                 soundid = string[-1]
             if linecount == 2:
                 datest = string[-1][0:4] + string[-1][5:7] + string[-1][8:10]
-                timest = string[-1][11:13] + string[-1][14:16] + string[-1][17:19]
+                timest = string[-1][11:13] + \
+                    string[-1][14:16] + string[-1][17:19]
             if linecount == 3:
                 serial = string[-1]
             if linecount == 4:
@@ -148,7 +155,8 @@ for ifile in range(0, len(filelist)):
 
     utctime = calendar.timegm(datetime.datetime(int(datest[0:4]), int(datest[4:6]), int(datest[6:8]),
                                                 int(timest[0:2]), int(timest[2:4]), int(timest[4:6])).timetuple())
-    data = np.genfromtxt(filelist[ifile], skip_header=9, invalid_raise=False, encoding="ISO-8859-1")
+    data = np.genfromtxt(
+        filelist[ifile], skip_header=9, invalid_raise=False, encoding="ISO-8859-1")
 
     num_rows = data.shape[0]  # length of raw array
     print('Number of rows: {}'.format(num_rows))
@@ -183,12 +191,15 @@ for ifile in range(0, len(filelist)):
     time_resolution = (np.diff(tindex)[np.argmax(np.bincount(indices))])
 
     # Create outputfile with time information from file
-    YYYYMM = num2date(np.int(utctime), 'seconds since 1970-01-01 00:00:00 UTC').strftime('%Y%m')
-    YYYYMMDDHHMM = num2date(np.int(utctime), 'seconds since 1970-01-01 00:00:00 UTC').strftime('%Y%m%d%H%M')
+    YYYYMM = num2date(
+        np.int(utctime), 'seconds since 1970-01-01 00:00:00 UTC').strftime('%Y%m')
+    YYYYMMDDHHMM = num2date(
+        np.int(utctime), 'seconds since 1970-01-01 00:00:00 UTC').strftime('%Y%m%d%H%M')
     outpath = args['outputfolder'] + YYYYMM + '/'
     if not os.path.exists(outpath):
         success = sp.call(["mkdir", "-p", outpath])
-    outfile = outpath + "rs__Vaisala__{}check6.nc".format(time_resolution, YYYYMMDDHHMM)
+    outfile = outpath + \
+        "rs__Vaisala__{}check6.nc".format(time_resolution, YYYYMMDDHHMM)
 
     # Creation of output NetCDF file
     fo = Dataset(outfile, 'w', format='NETCDF4')
@@ -213,7 +224,8 @@ for ifile in range(0, len(filelist)):
 
     # add file history, including file creation timestamp, path of python file and of ASCII data file
     fo.history = 'File created ' + time.ctime(time.time()) + ' with ' + os.path.basename(__file__), \
-                 ', last program modification on ' + time.ctime(os.path.getmtime(os.path.realpath(__file__)))
+                 ', last program modification on ' + \
+        time.ctime(os.path.getmtime(os.path.realpath(__file__)))
 
     # Define Dimension (record length) from ASCII record counter
     fo.createDimension('levels', num_rows)
@@ -223,37 +235,47 @@ for ifile in range(0, len(filelist)):
     nc_tindex = fo.createVariable('time', 'f4', ('levels'), fill_value=fillval)
     nc_tindex.long_name = 'time passed since launch'
     nc_tindex.units = 's'
-    nc_vvert = fo.createVariable('ascentRate', 'f4', ('levels'), fill_value=fillval)
+    nc_vvert = fo.createVariable(
+        'ascentRate', 'f4', ('levels'), fill_value=fillval)
     nc_vvert.long_name = 'Ascent/descent rate (vertical velocity)'
     nc_vvert.units = 'm/s'
-    nc_alti = fo.createVariable('altitude', 'f4', ('levels'), fill_value=fillval)
+    nc_alti = fo.createVariable(
+        'altitude', 'f4', ('levels'), fill_value=fillval)
     nc_alti.long_name = 'altitude'
     nc_alti.units = 'm'
-    nc_pres = fo.createVariable('pressure', 'f4', ('levels'), fill_value=fillval)
+    nc_pres = fo.createVariable(
+        'pressure', 'f4', ('levels'), fill_value=fillval)
     nc_pres.long_name = 'pressure'
     nc_pres.units = 'hPa'
-    nc_temp = fo.createVariable('temperature', 'f4', ('levels'), fill_value=fillval)
+    nc_temp = fo.createVariable(
+        'temperature', 'f4', ('levels'), fill_value=fillval)
     nc_temp.long_name = 'air temperature'
     nc_temp.units = 'degrees Celsius'
     nc_rh = fo.createVariable('humidity', 'f4', ('levels'), fill_value=fillval)
     nc_rh.long_name = 'relative humidity'
     nc_rh.units = '%'
-    nc_dewp = fo.createVariable('dewPoint', 'f4', ('levels'), fill_value=fillval)
+    nc_dewp = fo.createVariable(
+        'dewPoint', 'f4', ('levels'), fill_value=fillval)
     nc_dewp.long_name = 'dew point'
     nc_dewp.units = 'degrees Celsius'
-    nc_mix = fo.createVariable('mixingRatio', 'f4', ('levels'), fill_value=fillval)
+    nc_mix = fo.createVariable(
+        'mixingRatio', 'f4', ('levels'), fill_value=fillval)
     nc_mix.long_name = 'Water vapor mixing ratio'
     nc_mix.units = 'g/kg'
-    nc_vhori = fo.createVariable('windSpeed', 'f4', ('levels'), fill_value=fillval)
+    nc_vhori = fo.createVariable(
+        'windSpeed', 'f4', ('levels'), fill_value=fillval)
     nc_vhori.long_name = 'wind speed)'
     nc_vhori.units = 'm/s'
-    nc_vdir = fo.createVariable('windDirection', 'f4', ('levels'), fill_value=fillval)
+    nc_vdir = fo.createVariable(
+        'windDirection', 'f4', ('levels'), fill_value=fillval)
     nc_vdir.long_name = 'wind direction'
     nc_vdir.units = 'degrees'
-    nc_lat = fo.createVariable('latitude', 'f4', ('levels'), fill_value=fillval)
+    nc_lat = fo.createVariable(
+        'latitude', 'f4', ('levels'), fill_value=fillval)
     nc_lat.long_name = 'latitude'
     nc_lat.units = 'degrees-north'
-    nc_long = fo.createVariable('longitude', 'f4', ('levels'), fill_value=fillval)
+    nc_long = fo.createVariable(
+        'longitude', 'f4', ('levels'), fill_value=fillval)
     nc_long.long_name = 'longitude'
     nc_long.units = 'degrees-east'
 
