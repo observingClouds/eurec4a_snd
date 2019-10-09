7a60316c4a542b479b6af2969cc0497faa16ee10
hauke.schulz <43613877+observingClouds@users.noreply.github.com>
2019-10-08 22:34:29 -0700

    + Making the module directly excessible in the command line (hopefully)

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/observingClouds/eurec4a_snd",
     packages=setuptools.find_packages(),
+    py_modules=['eurec4a_snd'],
     setup_requires=['setuptools-git-version'],
     classifiers=[
         "Programming Language :: Python :: 3",
