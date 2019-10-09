efa2f8b0af0ae9d4dce852a7bee17253180429d1
hauke.schulz <43613877+observingClouds@users.noreply.github.com>
2019-10-08 22:05:50 -0700

    + Test pypi setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="eurec4a_snd",
+    version_format='{tag}.dev{commitcount}+{gitsha}',
+    author="eurec4a folks",
+    author_email="",
+    description="Common EUREC4A sounding standard",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/observingClouds/eurec4a_snd",
+    packages=setuptools.find_packages(),
+    setup_requires=['setuptools-git-version'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
