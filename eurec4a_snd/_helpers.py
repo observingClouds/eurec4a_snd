d63963a9b9d3e67b44bafbd172fc29755970ca32
Hauke Schulz <43613877+observingClouds@users.noreply.github.com>
2020-06-10 22:13:32 +0200

    / Fix: Setup logging

--- eurec4a_snd/_helpers.py
+++ eurec4a_snd/_helpers.py
@@ -34,11 +34,16 @@ def unixpath(path_in):
 
 def setup_logging(verbose):
     assert verbose in ["DEBUG", "INFO", "WARNING", "ERROR"]
+    # Get filename of calling script
+    frame = inspect.stack()[1]
+    module = inspect.getmodule(frame[0])
+    filename = module.__file__
+
     logging.basicConfig(
         level=logging.getLevelName(verbose),
         format="%(levelname)s - %(name)s - %(funcName)s - %(message)s",
         handlers=[
-            logging.FileHandler("{}.log".format(__file__)),
+            logging.FileHandler("{}.log".format(filename)),
             logging.StreamHandler()
         ])
 
